5617b04a40554227b637fbaf6ade1943ac3b1843
corvad <71835941+corvad@users.noreply.github.com>
2023-04-08 09:44:45 -0500

    Created form and model

--- recipevault/recipe/models.py
+++ recipevault/recipe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=20)
+    photo = models.ImageField()
+    rating = models.PositiveIntegerField()
+    description = models.TextField()
+    ingredients = models.TextField()
+    directions = models.TextField()
