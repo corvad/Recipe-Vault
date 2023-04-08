f419d68fc31c9d705421a8c800a5fc7401c87efc
corvad <71835941+corvad@users.noreply.github.com>
2023-04-08 10:27:32 -0500

    Added image processing

--- recipevault/recipe/models.py
+++ recipevault/recipe/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.urls import reverse
 
+
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=20)
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='images/')
     rating = models.PositiveIntegerField()
     description = models.TextField()
     ingredients = models.TextField()
     directions = models.TextField()
+
+    def __str__(self):
+        return self.name
