5a69e6b69d3eb5eea8527f5030d434d9b9e70200
corvad <71835941+corvad@users.noreply.github.com>
2023-04-08 15:42:00 -0500

    Fixed alert system

--- recipevault/recipe/views.py
+++ recipevault/recipe/views.py
@@ -1,3 +1,5 @@
+from django.urls import reverse
+from django.utils.html import format_html
 from .forms import RecipeForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
@@ -8,8 +10,10 @@ def add(request):
     if request.method == "POST":
         form = RecipeForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, ("Added recipe."))
+            temp = form.save()
+            ht = "/view/" + str(temp.id)
+            temp2 = format_html('Added recipe. <a href="{}">View Here</a>', reverse(ht))
+            messages.success(request, ("Added recipe."+ht))
         else:
             messages.error(request, ("Failed to add recipe, try again."))
     form = RecipeForm()
--- recipevault/recipevault/settings.py
+++ recipevault/recipevault/settings.py
@@ -82,6 +82,13 @@ DATABASES = {
     }
 }
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
